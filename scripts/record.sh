69d57f2428799ea9c290011784570389f0819318
Shengfan <csf990123@gmail.com>
2025-06-13 15:29:00 -0700

    Switching to dynamic bicycle model; comparison experiment with baseline

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,1 +1,1 @@
-ros2 bag record -o /home/$USER/Documents/francesco-racing/data/rosbags/$(date +%F_%H-%M-%S) /experiment/attacker_state_input_log /experiment/defender_state_input_log /experiment/barc_2/est_state /experiment/barc_3/est_state
+ros2 bag record -o /home/$USER/Documents/francesco-racing/data/rosbags/$(date +%F_%H-%M-%S) /experiment/attacker_state_input_log /experiment/defender_state_input_log 
