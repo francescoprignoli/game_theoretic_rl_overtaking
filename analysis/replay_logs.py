a4dbe08cbbb586dce4fe867c87533eeed73f9a0a
Shengfan <csf990123@gmail.com>
2025-06-17 16:41:08 -0700

    Minor improvements; experiments on 061725

--- analysis/replay_logs.py
+++ analysis/replay_logs.py
@@ -5,18 +5,22 @@ from pathlib import Path
 from mpcexp.utils.utils_fun import load_sim_data, dat2pkl, load_raw_data, unflatten_to_dict_of_lists
 
 
-def main(filename, replay_speed=1, save_video=False, root=None):
-
+def main(filename, replay_speed=1, save_video=False, root=None, unified=False):
     # ----- Load Data -----
-    att_data, att_size = load_raw_data(filename, root, player='attacker')
-    att_unflat_data = unflatten_to_dict_of_lists(att_data, log_size=att_size)
+    if not unified:
+        att_data, att_size = load_raw_data(filename, root, player='attacker')
+        att_unflat_data = unflatten_to_dict_of_lists(att_data, log_size=att_size)
 
-    def_data, def_size = load_raw_data(filename, root, player='defender')
-    def_unflat_data = unflatten_to_dict_of_lists(def_data, log_size=def_size)
+        def_data, def_size = load_raw_data(filename, root, player='defender')
+        def_unflat_data = unflatten_to_dict_of_lists(def_data, log_size=def_size)
 
-    size = min(att_size, def_size)
-    unflat_data = {k: v[:size] for k, v in att_unflat_data.items()}
-    unflat_data.update({k: v[:size] for k, v in def_unflat_data.items() if k != 'attacker_state'})
+        # size = min(att_size, def_size)
+
+        unflat_data = {k: v for k, v in att_unflat_data.items()}
+        unflat_data.update({k: v for k, v in def_unflat_data.items() if k != 'attacker_state'})
+    else:
+        data, size = load_raw_data(filename, root)
+        unflat_data = unflatten_to_dict_of_lists(data, log_size=size)
 
     # Fix key names
     # unflat_data["def"] = unflat_data.pop("log_def")
@@ -70,6 +74,11 @@ if __name__ == "__main__":
         action="store_true",
         help="Flag to save the animation as a video file (default: False)",
     )
+    parser.add_argument(
+        '--unified',
+        action='store_true',
+        help="Flag to indicate if the simulation data is unified (default: False)",
+    )
     args = parser.parse_args()
 
-    main(args.filename, args.replay_speed, args.save_video)
+    main(args.filename, args.replay_speed, args.save_video, unified=args.unified)
